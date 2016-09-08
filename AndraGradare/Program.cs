8fd55cde6044b1f2a5c549d2d0f7884be3230ea6
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2016-09-08 23:24:14 +0200

    Add files via upload

--- AndraGradare/Program.cs
+++ AndraGradare/Program.cs
@@ -11,26 +11,35 @@ namespace AndraGradare
 
         static double a, b, c, x1, x2;
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Useage: This can only be used for second degree equations. (Example: x^2 + 3x - 4 = 0)");
-            Console.WriteLine("What is your a value using this? ax2 + bx + c = 0");
-            a = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
-            Console.WriteLine("And your b value?");
-            b = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
-            Console.WriteLine("And your c value?");
-            c = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
-            Console.WriteLine("Calculating...");
-            CalculateX(a, b, c);
-
+        static void Main(string[] args) {
+        
+            while(true)
+            {
+                Console.WriteLine("Useage: This can only be used for second degree equations. (Example: x^2 + 3x - 4 = 0)");
+                Console.WriteLine("What is your a value using this? ax2 + bx + c = 0");
+                a = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
+                Console.WriteLine("And your b value?");
+                b = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
+                Console.WriteLine("And your c value?");
+                c = Double.Parse(Console.ReadLine()); //C# verkar inte gilla Console.Read så mycket så jag läser den som en string som jag konverterar till en double.
+                Console.WriteLine("Calculating...");
+                Console.WriteLine("\n");
+                Console.WriteLine("x1 = "+ CalculateX1(a, b, c));
+                if(CalculateX1(a, b, c) != CalculateX2(a, b, c))
+                    Console.WriteLine("x2 = " + CalculateX2(a, b, c));
+                Console.WriteLine("Please note, these might not be a real value.");
+                Console.WriteLine("\n");
+            }
         }
-        static void CalculateX(double a, double b, double c)
+        static double CalculateX1(double a, double b, double c)
         {
-                x1 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a); //Formeln för att räkna ut en av rötterna på en andragradsekvation 
-                x2 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a); //Formeln för att räkna ut den andra roten
-                Console.WriteLine("x1 = " + x1 + "\nx2 = " + x2); //Skriv ut rötterna
-                Console.WriteLine("Remember to check for false roots."); //Vi kollar inte om alla rötter stämmer
-                Console.Read();
+                x1 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a); //Formeln för att räkna ut en av rötterna på en andragradsekvation
+            return x1;
+        }
+        static double CalculateX2(double a, double b, double c)
+        {
+            x2 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a); //Formeln för att räkna ut den andra roten
+            return x2;
         }
     }
 }
